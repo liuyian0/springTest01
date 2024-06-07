f604c5e40a62270bae65563e81476b09b7e261c1
liu <1203733192@qq.com>
2024-06-07 10:17:52 +0800

    使用声明式事务实现转账业务

--- src/main/java/com/liu/config/SpringConfig4.java
+++ src/main/java/com/liu/config/SpringConfig4.java
@@ -7,16 +7,21 @@ public class SpringConfig4 {
 }*/
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 //优化Spring整合Mybatis
-@Configuration
+@Configurable
 @ComponentScan("com.liu")
 @PropertySource("classpath:db.properties")
 @Import(MyBatisConfig.class)
+//声明式事务第一步：开启事务管理支持
+@EnableTransactionManagement
 public class SpringConfig4{
     @Value("${jdbc.driver}")
     private String driver;
@@ -34,5 +39,12 @@ public class SpringConfig4{
         ds.setUsername(username);
         ds.setPassword(password);
         return ds;
+    }
+    //声明式事务第二步：配置事务管理器
+    @Bean
+    public DataSourceTransactionManager getTxManager(DataSource dataSource){
+        DataSourceTransactionManager manager = new DataSourceTransactionManager();
+        manager.setDataSource(dataSource);
+        return manager;
     }
 }
--- src/main/java/com/liu/service/AccountService2.java
+++ src/main/java/com/liu/service/AccountService2.java
@@ -1,6 +1,19 @@
 package com.liu.service;
+
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 //接口
 public interface AccountService2 {
+    //声明式事务第三步：配置需要事务支持的切入点
+    @Transactional(
+            isolation = Isolation.DEFAULT,
+            readOnly = false,
+            timeout = 10,
+            propagation = Propagation.REQUIRED
+    )
     //转账业务
     public abstract void transfer(int outId,int inId,double money);
+
 }
